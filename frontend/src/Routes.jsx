52935fd8d4a91246aad6510037db1cb481d0c6ad
Avishkar-s73 <avishkar.durgade.s73@kalvium.community>
2025-01-23 17:21:53 +0530

    milestone8

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,6 +1,7 @@
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
+import Home from "./pages/Home";
 
 export { LoginPage };
-export{SignupPage};
-
+export { SignupPage };
+export { Home };
