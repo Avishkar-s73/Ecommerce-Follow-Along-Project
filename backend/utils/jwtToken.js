66a46e519de61e604764698e09bd508f7585c99a
Avishkar-s73 <avishkar.durgade.s73@kalvium.community>
2025-01-23 15:29:14 +0530

    milestone7

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,19 +1,18 @@
-const sendToken=(user,statusCode,res)=>{
-    const token=user.getJwtToken();
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
 
-    const option={
-        expires: new Date(
-            Date.now()+90*5215012+8*1000
-        ),
-        httpOnly: true,
-    }
+  const option = {
+    expires: new Date(Date.now() + 90 * 5215012 + 8 * 1000),
+    httpOnly: true,
+    smaeSite: "none",
+    secure: true,
+  };
 
-    res.status(statusCode).cookie("token",token, option).json({
-        success:true,
-        token,
-        user
-    })
+  res.status(statusCode).cookie("token", token, option).json({
+    success: true,
+    token,
+    user,
+  });
+};
 
-}
-
-module.exports=sendToken;
+module.exports = sendToken;
